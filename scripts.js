75396f11f83440306160dfda1282217793d141bd
v4lkonn <miufix@gmail.com>
2025-08-27 18:15:59 +0800

    初版

---
+++ scripts.js
@@ -0,0 +1,172 @@
+function changeToNone(){
+    let meP=document.querySelector("#me");
+    let portfolioP=document.querySelector("#portfolio");
+    let scheduleP=document.querySelector("#schedule");
+    let futureP=document.querySelector("#future");
+    let skillP=document.querySelector("#skill");
+    let expsP=document.querySelector("#exps");
+    let viewP=document.querySelector("#view");
+    let etcP=document.querySelector("#etc");
+    meP.style.display="none";
+    portfolioP.style.display="none";
+    scheduleP.style.display="none";
+    futureP.style.display="none";
+    skillP.style.display="none";
+    expsP.style.display="none";
+    viewP.style.display="none";
+    etcP.style.display="none";
+}
+
+function changeToMe(){
+    let meP=document.querySelector("#me");
+    let portfolioP=document.querySelector("#portfolio");
+    let scheduleP=document.querySelector("#schedule");
+    let futureP=document.querySelector("#future");
+    let skillP=document.querySelector("#skill");
+    let expsP=document.querySelector("#exps");
+    let viewP=document.querySelector("#view");
+    let etcP=document.querySelector("#etc");
+    portfolioP.style.display="none";
+    scheduleP.style.display="none";
+    futureP.style.display="none";
+    skillP.style.display="none";
+    expsP.style.display="none";
+    viewP.style.display="none";
+    etcP.style.display="none";
+    meP.style.display="block";
+}
+
+function changeToPortfolio(){
+    let meP=document.querySelector("#me");
+    let portfolioP=document.querySelector("#portfolio");
+    let scheduleP=document.querySelector("#schedule");
+    let futureP=document.querySelector("#future");
+    let skillP=document.querySelector("#skill");
+    let expsP=document.querySelector("#exps");
+    let viewP=document.querySelector("#view");
+    let etcP=document.querySelector("#etc");
+    meP.style.display="none";
+    scheduleP.style.display="none";
+    futureP.style.display="none";
+    skillP.style.display="none";
+    expsP.style.display="none";
+    viewP.style.display="none";
+    etcP.style.display="none";
+    portfolioP.style.display="block";
+}
+
+function changeToSchedule(){
+    let meP=document.querySelector("#me");
+    let portfolioP=document.querySelector("#portfolio");
+    let scheduleP=document.querySelector("#schedule");
+    let futureP=document.querySelector("#future");
+    let skillP=document.querySelector("#skill");
+    let expsP=document.querySelector("#exps");
+    let viewP=document.querySelector("#view");
+    let etcP=document.querySelector("#etc");
+    meP.style.display="none";
+    portfolioP.style.display="none";
+    futureP.style.display="none";
+    skillP.style.display="none";
+    expsP.style.display="none";
+    viewP.style.display="none";
+    etcP.style.display="none";
+    scheduleP.style.display="block";
+}
+
+function changeToFuture(){
+    let meP=document.querySelector("#me");
+    let portfolioP=document.querySelector("#portfolio");
+    let scheduleP=document.querySelector("#schedule");
+    let futureP=document.querySelector("#future");
+    let skillP=document.querySelector("#skill");
+    let expsP=document.querySelector("#exps");
+    let viewP=document.querySelector("#view");
+    let etcP=document.querySelector("#etc");
+    meP.style.display="none";
+    portfolioP.style.display="none";
+    scheduleP.style.display="none";
+    skillP.style.display="none";
+    expsP.style.display="none";
+    viewP.style.display="none";
+    etcP.style.display="none";
+    futureP.style.display="block";
+}
+
+function changeToSkill(){
+    let meP=document.querySelector("#me");
+    let portfolioP=document.querySelector("#portfolio");
+    let scheduleP=document.querySelector("#schedule");
+    let futureP=document.querySelector("#future");
+    let skillP=document.querySelector("#skill");
+    let expsP=document.querySelector("#exps");
+    let viewP=document.querySelector("#view");
+    let etcP=document.querySelector("#etc");
+    meP.style.display="none";
+    portfolioP.style.display="none";
+    scheduleP.style.display="none";
+    futureP.style.display="none";
+    expsP.style.display="none";
+    viewP.style.display="none";
+    etcP.style.display="none";
+    skillP.style.display="block";
+}
+
+function changeToExps(){
+    let meP=document.querySelector("#me");
+    let portfolioP=document.querySelector("#portfolio");
+    let scheduleP=document.querySelector("#schedule");
+    let futureP=document.querySelector("#future");
+    let skillP=document.querySelector("#skill");
+    let expsP=document.querySelector("#exps");
+    let viewP=document.querySelector("#view");
+    let etcP=document.querySelector("#etc");
+    meP.style.display="none";
+    portfolioP.style.display="none";
+    scheduleP.style.display="none";
+    futureP.style.display="none";
+    skillP.style.display="none";
+    expsP.style.display="none";
+    viewP.style.display="none";
+    etcP.style.display="none";
+    expsP.style.display="block";
+}
+
+function changeToView(){
+    let meP=document.querySelector("#me");
+    let portfolioP=document.querySelector("#portfolio");
+    let scheduleP=document.querySelector("#schedule");
+    let futureP=document.querySelector("#future");
+    let skillP=document.querySelector("#skill");
+    let expsP=document.querySelector("#exps");
+    let viewP=document.querySelector("#view");
+    let etcP=document.querySelector("#etc");
+    meP.style.display="none";
+    portfolioP.style.display="none";
+    scheduleP.style.display="none";
+    futureP.style.display="none";
+    skillP.style.display="none";
+    expsP.style.display="none";
+    etcP.style.display="none";
+    viewP.style.display="block";
+}
+
+function changeToEtc(){
+    let meP=document.querySelector("#me");
+    let portfolioP=document.querySelector("#portfolio");
+    let scheduleP=document.querySelector("#schedule");
+    let futureP=document.querySelector("#future");
+    let skillP=document.querySelector("#skill");
+    let expsP=document.querySelector("#exps");
+    let viewP=document.querySelector("#view");
+    let etcP=document.querySelector("#etc");
+    meP.style.display="none";
+    portfolioP.style.display="none";
+    scheduleP.style.display="none";
+    futureP.style.display="none";
+    skillP.style.display="none";
+    expsP.style.display="none";
+    viewP.style.display="none";
+    etcP.style.display="block";
+}
+
